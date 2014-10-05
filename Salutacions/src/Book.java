10a9ba491401be21e9f06ab540a5b1b4a8206c22
sergimm <sergimillet@uoc.edu>
2014-10-05 17:14:24 +0200

    Add class

---
+++ Salutacions/src/Book.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * classe per fer més probes
+ *
+ */
+public class Book {
+	private String title;
+	private String author;
+	private String id;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+
+}
